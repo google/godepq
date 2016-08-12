aa6384f780e76e4f0862c30cbf610d07a1a65dde
Tim St. Clair <stclair@google.com>
2016-08-12 13:19:58 -0700

    Add testing package hierarchy

---
+++ testing/a/ab/imports.go
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2013-2016 the Godepq Authors
+
+Use of this source code is governed by a MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+*/
+
+package ab
+
+import (
+	_ "errors"
+)
---
+++ testing/c/imports.go
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2013-2016 the Godepq Authors
+
+Use of this source code is governed by a MIT-style
+license that can be found in the LICENSE file or at
+https://opensource.org/licenses/MIT.
+*/
+
+package c
+
+import (
+	_ "errors"
+)
